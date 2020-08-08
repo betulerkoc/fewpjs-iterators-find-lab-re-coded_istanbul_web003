605067f30110897d871eb31495845294357dac38
betul <bet.er@hotmail.com>
2020-08-08 22:46:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const testVar = {}
-
-function testFunc() {
-  return "hi"
+function superbowlWin(x) { 
+  const resultObject = x.find(s => s.result == "W");
+ // resultObject ? resultObject.year : undefined;
+  if(resultObject){
+      return resultObject.year
+    } else{
+      return undefined;
+    }
 }
